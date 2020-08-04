120432255cd9c538160dd1ba0bf93496e4ae6ad5
Ionică Bizău <bizauionica@gmail.com>
2020-08-04 16:43:34 +0300

    :arrow_up: 1.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,9 @@
 
 const downloadFileAuto = require("../lib");
 
-console.log(downloadFileAuto());
+// Even if the url does not has an extension,
+// the extension will be detected using the content-type header
+downloadFileAuto("https://example.com/banner", "profile_banner").then(p => {
+    console.log(`File saved as ${p}`)
+    // => File saved as profile_banner.jpeg
+}).catch(console.error)
